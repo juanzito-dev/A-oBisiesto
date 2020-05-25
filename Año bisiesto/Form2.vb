99f237709de512b3ec1ac6dd4ac1da2a5a5e100d
Juan Andrés Rodriguez <juan@null.net>
2020-05-25 18:16:47 -0300

    Subida

---
+++ Año bisiesto/Form2.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form2
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim Nombre As String
+        Nombre = Module1.nombre
+        Label3.Text = "¡BIENVENIDO " & Nombre + "!"
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim f3 As New Form3
+        Module1.año = Año.Text
+
+        Dim vacio, añoin As String
+
+        añoin = Año.Text
+        vacio = ""
+        If añoin <> vacio Then
+            f3.Show()
+            Me.Hide()
+        ElseIf Año.Text = vacio Then
+            MsgBox("Ingrese un Año")
+
+        End If
+    End Sub
+End Class
---
+++ Año bisiesto/Module1.vb
@@ -0,0 +1,4 @@
+﻿Module Module1
+    Public nombre As String
+    Public año As Integer
+End Module
